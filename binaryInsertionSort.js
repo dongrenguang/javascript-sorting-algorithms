60e5d5cc2c4fe4a7454906c852d4b9c4de4a38a1
Renguang Dong <dongrenguang@gmail.com>
2016-07-21 01:17:27 +0800

    Create binaryInsertionSort.

---
+++ binaryInsertionSort.js
@@ -0,0 +1,21 @@
+function binaryInsertionSort(array) {
+	var arrLength = array.length;
+	var i, low, high, middle, current;
+	for (i = 1; i < arrLength; i++) {
+		current = array.splice(i, 1)[0];
+		low = 0;
+		high = i - 1;
+		while (low <= high) {
+			middle = Math.floor((low + high) / 2);
+			if (current > array[middle]) {
+				low = middle + 1;
+			}
+			else {
+				high = middle - 1;
+			}
+		}
+		array.splice(low, 0, current);
+	}
+
+	return array;
+}
