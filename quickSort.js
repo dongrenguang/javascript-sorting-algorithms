c37447b5849c8b1184da7ad90a52d10497373552
Renguang Dong <dongrenguang@gmail.com>
2016-07-20 23:50:05 +0800

    Complete quickSort.

--- quickSort.js
+++ quickSort.js
@@ -7,18 +7,14 @@ function quickSort(array) {
 	var midValue = array.splice(midIndex, 1);
 	var left = [];
 	var right = [];
-
-	for (var i = 0; i < array.length; i++)
-	{
-		if (array[i] < midValue)
-		{
-			left.push(array[i]);
+	array.forEach(function(item) {
+		if (item < midValue) {
+			left.push(item);
 		}
-		else
-		{
-			right.push(array[i]);
+		else {
+			right.push(item);
 		}
-	}
+	});
 
 	return quickSort(left).concat(midValue).concat(quickSort(right));
 }
