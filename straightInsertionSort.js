35617b966b45948dd5eafe36c5d281b837373f5b
Renguang Dong <dongrenguang@gmail.com>
2016-07-21 00:25:38 +0800

    Create straightInsertionSort.

---
+++ straightInsertionSort.js
@@ -0,0 +1,11 @@
+function straightInsertionSort(array) {
+	var arrLength = array.length;
+	var i, j;
+	for (i = 1; i < arrLength; i++) {
+		var current = array.splice(i, 1)[0];
+		for (j = i - 1; array[j] > current && j >= 0; j--) {}
+		array.splice(j + 1, 0, current);
+	}
+
+	return array;
+}
