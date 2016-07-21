5c08ba0978b4bd7afd2801745c4e3ebf9cb86d1c
Renguang Dong <dongrenguang@gmail.com>
2016-07-21 23:40:19 +0800

    Complete bubbleSort.

---
+++ bubbleSort.js
@@ -0,0 +1,21 @@
+function bubbleSort(array) {
+	var arrLength = array.length;
+	var i, j, temp, bubbled;
+	for (i = 0; i < arrLength; i++) {
+		bubbled = false;
+		for (j = arrLength; j > i; j--) {
+			if (array[j] < array[j - 1]) {
+				temp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
+				bubbled = true;
+			}
+		}
+
+		if (!bubbled) {
+			break;
+		}
+	}
+
+	return array;
+}
