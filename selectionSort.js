610540387aacbced68b687ffa5ed4c8a0a9cc6ca
Renguang Dong <dongrenguang@gmail.com>
2016-07-21 23:58:41 +0800

    Complete selectionSort.

---
+++ selectionSort.js
@@ -0,0 +1,20 @@
+function selectionSort(array) {
+	var arrLength = array.length;
+	var i, j, min, temp;
+	for (i = 0; i < arrLength; i++) {
+		min = i;
+		for (j = i + 1; j < arrLength; j++) {
+			if (array[j] < array[min]) {
+				min = j;
+			}
+		}
+
+		if (min !== i) {
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+		}
+	}
+
+	return array;
+}
