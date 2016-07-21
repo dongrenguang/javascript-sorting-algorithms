900650e61e5debc0c854cfd2c717603f27f470f9
Renguang Dong <dongrenguang@gmail.com>
2016-07-22 01:07:12 +0800

    Add standard version of quickSort.

--- quickSort.js
+++ quickSort.js
@@ -1,3 +1,34 @@
+// standard version
+function quickSort(array, low, high) {
+	low = (low === undefined) ? 0 : low;
+	high = (high === undefined) ? (array.length - 1) : high;
+	if (low < high) {
+		var pivotpos = partition(array, low, high);
+		(function(_pivotpos, _low, _high) {
+			quickSort(array, _low, _pivotpos - 1);
+			quickSort(array, _pivotpos + 1, _high);
+		})(pivotpos, low, high);
+	}
+
+	return array;
+}
+
+function partition(array, low, high) {
+	var pivot = array[low];
+	while(low < high) {
+		while(low < high && array[high] >= pivot) { high--; }
+		array[low] = array[high];
+
+		while(low < high && array[low] <= pivot) { low++; }
+		array[high] = array[low];
+	}
+
+	array[low] = pivot;
+	return low;
+}
+
+/****************************************************************************/
+// another version
 function quickSort(array) {
 	if (array.length <= 1)
 	{
