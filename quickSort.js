360e42600ca027e1af973001e5e789086dd4196c
Renguang Dong <dongrenguang@gmail.com>
2016-07-22 20:34:01 +0800

    Code format.

--- quickSort.js
+++ quickSort.js
@@ -1,51 +1,51 @@
 // standard version
 function quickSort(array, low, high) {
-	low = (low === undefined) ? 0 : low;
-	high = (high === undefined) ? (array.length - 1) : high;
-	if (low < high) {
-		var pivotpos = partition(array, low, high);
-		(function(_pivotpos, _low, _high) {
-			quickSort(array, _low, _pivotpos - 1);
-			quickSort(array, _pivotpos + 1, _high);
-		})(pivotpos, low, high);
-	}
+    low = (low === undefined) ? 0 : low;
+    high = (high === undefined) ? (array.length - 1) : high;
+    if (low < high) {
+        var pivotpos = partition(array, low, high);
+        (function(_pivotpos, _low, _high) {
+            quickSort(array, _low, _pivotpos - 1);
+            quickSort(array, _pivotpos + 1, _high);
+        })(pivotpos, low, high);
+    }
 
-	return array;
+    return array;
 }
 
 function partition(array, low, high) {
-	var pivot = array[low];
-	while(low < high) {
-		while(low < high && array[high] >= pivot) { high--; }
-		array[low] = array[high];
+    var pivot = array[low];
+    while(low < high) {
+        while(low < high && array[high] >= pivot) { high--; }
+        array[low] = array[high];
 
-		while(low < high && array[low] <= pivot) { low++; }
-		array[high] = array[low];
-	}
+        while(low < high && array[low] <= pivot) { low++; }
+        array[high] = array[low];
+    }
 
-	array[low] = pivot;
-	return low;
+    array[low] = pivot;
+    return low;
 }
 
 /****************************************************************************/
 // another version
 function quickSort(array) {
-	if (array.length <= 1)
-	{
-		return array;
-	}
-	var midIndex = Math.round(array.length / 2);
-	var midValue = array.splice(midIndex, 1);
-	var left = [];
-	var right = [];
-	array.forEach(function(item) {
-		if (item < midValue) {
-			left.push(item);
-		}
-		else {
-			right.push(item);
-		}
-	});
+    if (array.length <= 1)
+    {
+        return array;
+    }
+    var midIndex = Math.round(array.length / 2);
+    var midValue = array.splice(midIndex, 1);
+    var left = [];
+    var right = [];
+    array.forEach(function(item) {
+        if (item < midValue) {
+            left.push(item);
+        }
+        else {
+            right.push(item);
+        }
+    });
 
-	return quickSort(left).concat(midValue).concat(quickSort(right));
+    return quickSort(left).concat(midValue).concat(quickSort(right));
 }
--- selectionSort.js
+++ selectionSort.js
@@ -1,20 +1,20 @@
 function selectionSort(array) {
-	var arrLength = array.length;
-	var i, j, min, temp;
-	for (i = 0; i < arrLength; i++) {
-		min = i;
-		for (j = i + 1; j < arrLength; j++) {
-			if (array[j] < array[min]) {
-				min = j;
-			}
-		}
+    var arrLength = array.length;
+    var i, j, min, temp;
+    for (i = 0; i < arrLength; i++) {
+        min = i;
+        for (j = i + 1; j < arrLength; j++) {
+            if (array[j] < array[min]) {
+                min = j;
+            }
+        }
 
-		if (min !== i) {
-			temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
-		}
-	}
+        if (min !== i) {
+            temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+    }
 
-	return array;
+    return array;
 }
